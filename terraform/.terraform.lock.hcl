3c515812938d4d69e1a5feebffa3d74ff1dba128
newswim <ofenixculpa@gmail.com>
2022-10-20 20:59:31 -0500

    add terraform project

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.2.0"
+  hashes = [
+    "h1:2K5LQkuWRS2YN1/YoNaHn9MAzjuTX8Gaqy6i8Mbfv8Y=",
+    "zh:06bd875932288f235c16e2237142b493c2c2b6aba0e82e8c85068332a8d2a29e",
+    "zh:0c681b481372afcaefddacc7ccdf1d3bb3a0c0d4678a526bc8b02d0c331479bc",
+    "zh:100fc5b3fc01ea463533d7bbfb01cb7113947a969a4ec12e27f5b2be49884d6c",
+    "zh:55c0d7ddddbd0a46d57c51fcfa9b91f14eed081a45101dbfc7fd9d2278aa1403",
+    "zh:73a5dd68379119167934c48afa1101b09abad2deb436cd5c446733e705869d6b",
+    "zh:841fc4ac6dc3479981330974d44ad2341deada8a5ff9e3b1b4510702dfbdbed9",
+    "zh:91be62c9b41edb137f7f835491183628d484e9d6efa82fcb75cfa538c92791c5",
+    "zh:acd5f442bd88d67eb948b18dc2ed421c6c3faee62d3a12200e442bfff0aa7d8b",
+    "zh:ad5720da5524641ad718a565694821be5f61f68f1c3c5d2cfa24426b8e774bef",
+    "zh:e63f12ea938520b3f83634fc29da28d92eed5cfbc5cc8ca08281a6a9c36cca65",
+    "zh:f6542918faa115df46474a36aabb4c3899650bea036b5f8a5e296be6f8f25767",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.0.2"
+  constraints = "~> 3.0.2"
+  hashes = [
+    "h1:BHD9F0IpzHOPgvFvzUbOx+FhzfNkBdrLanutWwHSCOs=",
+    "zh:4d7dcf1318f23202a33ac1e7dbed692ec80d86813fc50149bd46863e6c9584eb",
+    "zh:64171745d17f10d9cc60ae77443b5439a84fb3cc69d164f3ffe527c5f271e6da",
+    "zh:7bb19cbb5702b19ae010af3f8b8c949b183748e1859165e7268d00ea62f4e460",
+    "zh:7f6e634e9d9c5b43a4ff36f8797f51f00fa13df36361a80f3ca7482349279963",
+    "zh:84f64143e66f627b035fef9345271059d3ce51fea2eb4c52188647ef01124a82",
+    "zh:89ce9fc1a4ec40a0904b68a24fe1e598a4a89d42fc91772c89de57a88c862890",
+    "zh:96b610037db94ae23e7f73ba3296164d761319b7943fc7443f63029e3db7cc9b",
+    "zh:a216690bf0d33b43580ba80088dcc4cb3b39fa0c8eb0e9d221d9f02ca39df41b",
+    "zh:b4f39b1b37d9b29230e36c00e825f279337b2bb9e53b3d3b1ee0544782794606",
+    "zh:b686d9615d2627e1631e6cc07b1a61d9865ea43db8042ea5365fcc9ac5b1f465",
+    "zh:bc96d3bc3d122f81fea271bba5104ba32860d5e84f03da19751e5e8cb883ebef",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,114 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+
+    archive = {}
+  }
+
+  required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+    resource_group_name  = "Indigent-Defense-Resource-Group"
+    storage_account_name = "indigentdefense"
+    container_name       = "dev-terraform-state" # can't use envrionment var here :(
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "archive" {}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "id-${var.environment}-resource-group"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "id${var.environment}storage"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_application_insights" "application_insights" {
+  name                = "id-${var.environment}-application-insights"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  application_type    = "other"
+}
+
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "id-${var.environment}-app-service-plan"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                 = "id-function-app-src"
+  storage_account_name = azurerm_storage_account.storage_account.name
+
+}
+
+data "archive_file" "file_scraper_function" {
+  type        = "zip"
+  source_dir  = "../scraper"
+  output_path = "scraper.zip"
+}
+
+resource "azurerm_storage_blob" "storage_blob" {
+  name                   = "${filesha256(data.archive_file.file_scraper_function.output_path)}.zip"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+  source                 = data.archive_file.file_scraper_function.output_path
+}
+
+data "azurerm_storage_account_blob_container_sas" "storage_account_blob_container_sas" {
+  connection_string = azurerm_storage_account.storage_account.primary_connection_string
+  container_name    = azurerm_storage_container.storage_container.name
+
+  start  = "2022-01-01T00:00:00Z"
+  expiry = "2025-01-01T00:00:00Z"
+
+  permissions {
+    read   = true
+    add    = false
+    create = false
+    write  = false
+    delete = false
+    list   = false
+  }
+}
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                = "id-${var.environment}-function-app"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
+  # this is where we'll merge all of the environment variables
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE"       = "https://${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.storage_container.name}/${azurerm_storage_blob.storage_blob.name}${data.azurerm_storage_account_blob_container_sas.storage_account_blob_container_sas.sas}",
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
+  }
+
+  site_config {
+    application_stack {
+      python_version = "3.8"
+    }
+  }
+  storage_account_name        = azurerm_storage_account.storage_account.name
+  storage_account_access_key  = azurerm_storage_account.storage_account.primary_access_key
+  functions_extension_version = "~4"
+
+}
