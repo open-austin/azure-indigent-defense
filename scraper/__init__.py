7798f192807164e6a2e9f181a91d1d2b58e555f5
newswim <ofenixculpa@gmail.com>
2022-10-20 20:59:46 -0500

    use date defaults

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -7,13 +7,14 @@ import requests
 from bs4 import BeautifulSoup
 import azure.functions as func
 
-from shared.arguments import arg_parser
 from shared.helpers import *
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    start_date = date.today() - timedelta(days=30)
-    end_date = date.today()
+    start_date = date.fromisoformat(
+        os.getenv("start_date", str(date.today() - timedelta(days=1)))
+    )
+    end_date = date.fromisoformat(os.getenv("end_date", str(date.today())))
     county = os.getenv("county", "hays")
     judicial_officers = os.getenv("judicial_officers", "").split(":")
     ms_wait = int(os.getenv("ms_wait", "200"))
