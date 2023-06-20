9050a8264c807790c7597799cb76f3046bf52225
Kara Johnson <kjj8781@gmail.com>
2023-06-20 12:49:43 -0500

    Fast API configured and deployed

---
+++ wrapper-function/__init__.py
@@ -0,0 +1,21 @@
+import logging
+import azure.functions as func
+from FastAPIApp import app  # Main API application
+
+
+@app.get("/sample")
+async def index():
+    return {
+        "info": "Try /hello/K for parameterized route.",
+    }
+
+
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
+
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(app).handle_async(req, context)
